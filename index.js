fb6243e1e364cb853670ca2204cd32a9ec49529e
Anh Dang <anhcdang@gmail.com>
2021-07-20 21:33:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+        name: name,
+        email: email
+    }
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then(resp => resp.json())
+    .then(json => document.body.append(json.id))
+    .catch(error => document.body.append(error.message))
+}
